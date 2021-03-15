f2c1e18f0f18b73cd4b8c0b8b806fbacacbeaaa2
DonPandos <ewrr_96@mail.ru>
2021-03-15 17:10:46 +0300

    Main class documented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,26 @@ package org.example;
 
 import org.example.controllers.MainPaneController;
 
+/**
+ * Главный класс приложения
+ *
+ * Класс запускает главное окно приложения
+ *
+ * @author bogdan
+ * @version 1.0.1
+ * @copyright GNU Public License
+ * @todo Реализовать все методы
+ */
 public class Main {
+
+    /**
+     * 
+     * Создается объект класса MainPaneController и вызывается метод,
+     * который отображает главное окно приложения
+     *
+     * @param args аргументы приложения
+     * @return void
+     */
     public static void main(String[] args) {
         MainPaneController mainPaneController = new MainPaneController();
         mainPaneController.startMainPane();
